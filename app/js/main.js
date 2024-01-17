0b84fae22a303e54aa3b626a2e34a68aec063daf
NikolayShavar <fargon45821@gmail.com>
2024-01-17 16:54:56 +0200

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-$(".single-item").slick({
+$(".single-item1").slick({
   infinite: true,
   fade: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   prevArrow: $('.arrow-after.projects-before'),
   nextArrow: $('.arrow-next.projects-next'),
+});
+
+$(".single-item2").slick({
+  infinite: true,
+  fade: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: $('.arrow-after.development-previous'),
+  nextArrow: $('.arrow-next.development-next'),
 });
